9da86e35a7a5dffe72ecb485b8ed88cfdc1fa496
makson <maskbatkaev06@gmail.com>
2025-12-13 21:39:34 +0300

    init commit

---
+++ src/main/java/org/makson/guardbot/GuardBotApplication.java
@@ -0,0 +1,13 @@
+package org.makson.guardbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuardBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/makson/guardbot/GuardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.makson.guardbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
