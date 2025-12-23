3b5df4060fe112e9b96cb2262fb36393e99c62a3
makson <maskbatkaev06@gmail.com>
2025-12-23 19:23:35 +0300

    add data

--- src/main/resources/db/changelog/db.changelog-2.0.sql
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -15,8 +15,20 @@ VALUES ('Стажёр', 0, 0, 1),
        ('Глава гвардии', -1, -1, 8);
 
 INSERT INTO guardsmen(name, rank_id, points, special_report, last_report)
-VALUES ('Faust_Bismarkvil', 8, -1, -1, null),
-       ('Mayzito', 7, -1, -1, null);
+VALUES ('Faust_Bismarkvil', 8, 0, 0, null),
+       ('Mayzito', 7, 0, 0, null),
+       ('_A1d', 2, 0, 0, null),
+       ('KoftachkaWTF', 1, 0, 0, null),
+       ('Olenik636', 1, 0, 0, null),
+       ('MaksonLegend', 2, 17, 0, '2025-12-13'),
+       ('Chertila13', 1, 0, 0, '2025-12-13'),
+       ('Ay_ro_', 3, 30, 0, '2025-12-11'),
+       ('Nick_Torch14', 6, 0, 0, '2025-12-11'),
+       ('Su4cka', 2, 0, 0, null),
+       ('Charlotan', 2, 7, 0, '2025-12-9'),
+       ('Aleksandlolkek', 1, 0, 0, '2025-12-8'),
+       ('MaksMaruS_', 2, 0, 0, null);
+
 INSERT INTO department_members(guardsman_id, department_id, role)
 VALUES (1, 4, 'HEADMAN'),
        (2, 4, 'HEADMAN')
